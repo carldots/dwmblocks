e3a657e0e50b4509a4cf69058315c3a766fd74f1
carldots <carldots@protonmail.com>
2024-07-04 11:10:45 +0200

    Changed device inputs to match labpc

--- blocks.def.h
+++ blocks.def.h
@@ -6,9 +6,11 @@ static const Block blocks[] = {
 
   {"", "echo -n \" \" && curl -Ss 'https://wttr.in/halmstad?0&T&Q' | cut -c 16- | head -2 | xargs", 100, 0},
 /*  {"", "df -h -P /dev/sda1 | awk '{print $4}' | awk 'NR==2' | sed 's/^/AUX: /'", 5, 0}, */
-  {"", "df -h -P /dev/nvme1n1p2 | awk '{print $4}' | awk 'NR==2' | sed 's/^/ROOT: /'", 5, 0},
+/*  {"", "df -h -P /dev/sda1 | awk '{print $4}' | awk 'NR==2' | sed 's/^/AUX: /'", 5, 0}, */
+/*  {"", "df -h -P /dev/nvme1n1p2 | awk '{print $4}' | awk 'NR==2' | sed 's/^/ROOT: /'", 5, 0}, */
+  {"", "df -h -P /dev/dm-0 | awk '{print $4}' | awk 'NR==2' | sed 's/^/ROOT: /'", 5, 0},
   {"", "echo -n \"MEM: \" && free -h --giga | awk '/^Mem/ {print $3}'", 1, 0},
-  {"", "date", 1, 0}
+  {"", "date", 1, 0},
 
 
 };
