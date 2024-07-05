433779b03dbead73fb412f09e9879c1248e49fd8
carldots <carldots@protonmail.com>
2024-07-05 09:32:34 +0200

    Changed weather curl to a saner pull interval, 500s

--- blocks.def.h
+++ blocks.def.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	/*{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
 
 
-  {"", "echo -n \" \" && curl -Ss 'https://wttr.in/halmstad?0&T&Q' | cut -c 16- | head -2 | xargs", 100, 0},
+  {"", "echo -n \" \" && curl -Ss 'https://wttr.in/halmstad?0&T&Q' | cut -c 16- | head -2 | xargs", 500, 0},
 /*  {"", "df -h -P /dev/sda1 | awk '{print $4}' | awk 'NR==2' | sed 's/^/AUX: /'", 5, 0}, */
 /*  {"", "df -h -P /dev/sda1 | awk '{print $4}' | awk 'NR==2' | sed 's/^/AUX: /'", 5, 0}, */
 /*  {"", "df -h -P /dev/nvme1n1p2 | awk '{print $4}' | awk 'NR==2' | sed 's/^/ROOT: /'", 5, 0}, */
